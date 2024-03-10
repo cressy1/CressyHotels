c3382ed16b13551bc285ea19492bcb630f4c1923
cressy1 <martins.agbadamashi@decagon.dev>
2024-03-10 12:36:37 +0100

    Boiler plate 2

---
+++ src/main/java/com/kingslandinghotelandsuites/kingslandinghotelandsuites/response/BookingResponse.java
@@ -0,0 +1,33 @@
+package com.kingslandinghotelandsuites.kingslandinghotelandsuites.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BookingResponse {
+    private Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int NumOfAdults;
+    private int NumOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private RoomResponse room;
+
+    public BookingResponse(Long bookingId, LocalDate checkInDate,
+                           LocalDate checkOutDate,
+                           String bookingConfirmationCode) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+}
